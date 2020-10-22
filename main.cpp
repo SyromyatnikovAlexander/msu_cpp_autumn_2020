c03243d841ef85e1b747b6d494085b7a11efcecb
SyromyatnikovAlexander <72140706+SyromyatnikovAlexander@users.noreply.github.com>
2020-10-22 15:50:03 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <cstddef>
+#include "allocator.h"
+
+void Allocator::makeAllocator(std::size_t maxSize) {
+    if(init != nullptr) {
+        delete[] init;
+    }
+    mySize = maxSize;
+    init = new char[mySize];
+    offset = init;
+}
+
+char* Allocator::alloc(std::size_t size) {
+    if(size > mySize - (offset - init)) {
+        return nullptr;
+    }
+    offset += size;
+    return offset - size;
+}
+    
+void Allocator::reset() {
+    offset = init;
+}
+
